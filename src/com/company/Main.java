e56ab174a72d39cc0b0a7eac5b33c470d76d55d8
Yadviga.M <yme@tassta.com>
2022-06-03 19:41:30 +0300

    Added new commit 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hi, QA.GURU! New commit №2");
+        System.out.println("Hi, QA.GURU! New commit №3");
     }
 }
